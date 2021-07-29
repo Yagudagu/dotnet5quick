b6e4aa4045b667a4c9ecb786e7c1398925de3a88
Yagudagu <Jacobhmartian@gmail.com>
2021-07-29 17:45:41 +0000

    finally I think

--- Startup.cs
+++ Startup.cs
@@ -47,7 +47,7 @@ namespace dotnet5quick
             app.UseStaticFiles(new StaticFileOptions 
             {
                 FileProvider = new PhysicalFileProvider(
-                    Path.Combine(env.ContentRootPath, ".")),
+                    Path.Combine(env.ContentRootPath, "wwwroot")),
                 RequestPath = "/version"
             });
 
