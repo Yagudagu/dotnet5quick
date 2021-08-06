7e55d089a858d12cec9eef76c65d3a7f7db10f1e
Yagudagu <Jacobhmartian@gmail.com>
2021-08-06 14:01:28 +0000

    delete the big web.config

---
+++ bin/release/net5.0/publish/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+console.log(
+  "major changes 4, electric boogaloo, lets try here too I must believe"
+);
