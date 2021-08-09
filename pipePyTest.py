d966d098f3c7d04a73ba4064b36a01b6bc9a88f0
Yagudagu <Jacobhmartian@gmail.com>
2021-08-09 20:59:56 +0000

    doint it

--- pipePyTest.py
+++ pipePyTest.py
@@ -2,12 +2,14 @@ import os
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
-# credential = ManagedIdentityCredential()
-# credentail = credential.get_token()
+credential = ManagedIdentityCredential()
+print("credential: " + credential)
+token = credential.get_token(scopes='6aa6c4c3-6fba-4f2d-ab09-9035c0e40c19/.default')
+print(token)
 # request = HttpRequest(
 #   method='POST',
 #   url='https://jacmarauthfunctest.azurewebsites.net/api/HttpTrigger1?code=CaDqYH0lM9N4zKjUBgZfiC/5Jlkf/nGDeq7yC0yZ0qL6cdDjfxIV5A==',
-#   headers={"authorization":},
+#   headers={"authorization": 'Bearer ' + token},
 #   body={}
 #   )
             
