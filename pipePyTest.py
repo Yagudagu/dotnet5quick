f756cc59a7711a30fc0f3e9e424a28230032d9e1
Yagudagu <Jacobhmartian@gmail.com>
2021-08-09 21:07:15 +0000

    fixing issues

--- pipePyTest.py
+++ pipePyTest.py
@@ -1,5 +1,5 @@
 import os
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
 from azure.keyvault.secrets import SecretClient
 
 credential = ManagedIdentityCredential()
