74de897a8f1c0077f3502b282ea9d6bf45d40447
Yagudagu <Jacobhmartian@gmail.com>
2021-08-10 17:00:17 +0000

    almost there

--- pipePyTest.py
+++ pipePyTest.py
@@ -5,12 +5,13 @@ from azure.keyvault.secrets import SecretClient
 credential = DefaultAzureCredential()
 token = credential.get_token('4815043d-dd36-4ffd-80fb-1e5736923579/.default')
 print(token)
-# request = HttpRequest(
-#   method='POST',
-#   url='https://jacmarauthfunctest.azurewebsites.net/api/HttpTrigger1?code=CaDqYH0lM9N4zKjUBgZfiC/5Jlkf/nGDeq7yC0yZ0qL6cdDjfxIV5A==',
-#   headers={"authorization": 'Bearer ' + token},
-#   body={}
-#   )
+
+request = HttpRequest(
+  method='POST',
+  url='https://jacmarauthfunctest.azurewebsites.net/api/HttpTrigger1?code=CaDqYH0lM9N4zKjUBgZfiC/5Jlkf/nGDeq7yC0yZ0qL6cdDjfxIV5A==',
+  headers={"authorization": 'Bearer ' + token},
+  body={"name":"jacob"}
+  )
             
 print("made it here")
 #print(request)
